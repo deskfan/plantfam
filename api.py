f63bbbb2eecb8532b970d1dc6a4d704de6b24529
deskfan <40151988+deskfan@users.noreply.github.com>
2021-07-16 06:40:05 -0300

    Added Care endpoint using sqlalchemy

--- api.py
+++ api.py
@@ -3,11 +3,16 @@ from flask import Flask, request, jsonify
 from flask_restful import Resource, Api
 import mysql.connector
 from credentials import creds
+from classes import PlantCareType, PlantInventory, PlantHistory, PlantSpecies
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+import json
 
 app = Flask(__name__)
 api = Api(app)
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 
+CONN_STR = f"mysql://{creds['user']}:{creds['password']}@{creds['host']}/{creds['database']}"
 
 
 class Inventory(Resource):
@@ -36,8 +41,33 @@ class Inventory(Resource):
             return False
 
 
+class Care(Resource):
+    def get(self):
+        engine = create_engine(CONN_STR, echo=True)
+
+        Session = sessionmaker()
+        Session.configure(bind=engine)
+        session = Session()
+
+
+        query = session.query(PlantHistory).\
+            join(PlantInventory).\
+            join(PlantSpecies).\
+            join(PlantCareType).\
+            filter(PlantInventory.site_user_id==1).all()
+
+
+        my_list = []
+        for row in query:
+            x = row.to_json()
+            my_list.append(x)
+
+            x = json.dumps(my_list)
+
+        return x
 
 api.add_resource(Inventory, '/Inventory')
+api.add_resource(Care, '/Care')
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ classes.py
@@ -0,0 +1,68 @@
+from sqlalchemy import create_engine #, MetaData, Table, select, insert
+from sqlalchemy import Column, Integer, String, ForeignKey #, Sequence
+from sqlalchemy.orm import sessionmaker, relationship
+from sqlalchemy.ext.declarative import declarative_base
+#from sqlalchemy.ext.declarative import DeclarativeMeta
+from sqlalchemy.sql.sqltypes import Date
+import json
+
+
+Base = declarative_base()
+
+class PlantSpecies(Base):
+    __tablename__ = 'plant_species'
+    plant_species_id = Column(Integer, primary_key=True)
+    plant_botanical_name = Column(String)
+    plant_common_name = Column(String)
+
+    def __repr__(self):
+        return "<Plant(id='%s', botanical='%s', common='%s')>" % (
+                            self.plant_species_id, self.plant_botanical_name, self.plant_common_name)
+
+    def to_json(self):
+        return {'id':self.plant_species_id, 'bot':self.plant_botanical_name,'com':self.plant_common_name}
+
+
+class PlantCareType(Base):
+    __tablename__ = 'plant_care_types'
+    plant_care_type_id = Column(Integer, primary_key=True)
+    plant_care_type = Column(String)
+    sort_order = Column(Integer)
+
+    def __repr__(self):
+        return "<Care(id='%s', type='%s', sort='%s')>" % (
+                            self.plant_care_type_id, self.plant_care_type, self.sort_order)
+
+    def to_json(self):
+        return {'id':self.plant_care_type_id, 'type':self.plant_care_type,'sort':self.sort_order}
+
+
+class PlantInventory(Base):
+    __tablename__ = 'plant_inventory'
+    plant_inventory_id = Column(Integer, primary_key=True)
+    site_user_id = Column(Integer)
+    plant_species_id = Column(Integer, ForeignKey('plant_species.plant_species_id'))
+    species = relationship("PlantSpecies")
+
+    def to_json(self):
+        return {'id':self.plant_inventory_id, 'species':self.species.to_json()}
+
+
+
+class PlantHistory(Base):
+    __tablename__ = 'plant_history'
+    plant_history_id = Column(Integer, primary_key=True)
+    notes = Column(String)
+    plant_inventory_id = Column(Integer, ForeignKey('plant_inventory.plant_inventory_id'))
+    plant_care_type_id = Column(Integer, ForeignKey('plant_care_types.plant_care_type_id'))
+    plant_care_time = Column(Date)
+    inventory = relationship("PlantInventory")
+    care = relationship("PlantCareType")
+
+
+    def __repr__(self):
+        return "<History(id='%s', species_id='%s', species='%s')>" % (
+                            self.plant_history_id, self.care, self.inventory.species)
+
+    def to_json(self):
+        return {'id':self.plant_history_id,'species':self.inventory.to_json(),'care': self.care.to_json()}
