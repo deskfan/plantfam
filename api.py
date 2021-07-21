1041727f80ec461690fdcb257c69c42d91db6426
deskfan <40151988+deskfan@users.noreply.github.com>
2021-07-21 08:56:55 -0300

    namespace and swagger docs sorted

--- api.py
+++ api.py
@@ -1,14 +1,21 @@
 from flask import Flask
-from flask_restx import Api, Resource, fields
+import webargs as wa
+import webargs.flaskparser as wf
 
+from werkzeug.middleware.proxy_fix import ProxyFix
+from flask_restx import Api, Resource, fields
 from credentials import creds
 from classes import PlantCareType, PlantInventory, PlantHistory, PlantSpecies
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 
 app = Flask(__name__)
+app.wsgi_app = ProxyFix(app.wsgi_app)
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
-api = Api(app)
+api = Api(app, version='1.0', title='PlantFam API',
+    description='Logging plant care events for plant inventory')
+
+ns = api.namespace('plantfam', description='Plant care operations')
 
 CONN_STR = f"mysql://{creds['user']}:{creds['password']}@{creds['host']}/{creds['database']}"
 engine = create_engine(CONN_STR, echo=True)
@@ -16,24 +23,24 @@ Session = sessionmaker()
 Session.configure(bind=engine)
 session = Session()
 
-model_care = api.model('CareTwo',{
+model_care = api.model('CareTypes',{
                                 'care_type_id' : fields.Integer,
                                 'sort' : fields.Integer,
                                 'type' : fields.String
                             })
 
-model_species = api.model('SpeciesTwo',{
+model_species = api.model('Species',{
                                     'bot' : fields.String,
                                     'com' : fields.String,
                                     'species_id' : fields.Integer
                                 })
 
-model_history = api.model('HistoryTwo',{
+model_history = api.model('History',{
                                 'inventory_id' : fields.Integer,
                                 'species': fields.Nested(model_species)
                                 })
 
-model_nested = api.model('Care', {
+model_nested = api.model('CareHistory', {
                             'care' : fields.Nested(model_care),
                             'history_id' : fields.Integer,
                             'species' : fields.Nested(model_history)
@@ -41,27 +48,28 @@ model_nested = api.model('Care', {
                     )
 
 
-care_fields = {'care_type_id': fields.Integer}
-#care_fields['sort'] = fields.Integer
-care_fields['type'] = fields.String
+user_fields = api.model('User', {'user_id': fields.Integer})
 
 
-@api.route('/CareTypes')
+@ns.route('/CareTypes')
 class CareTypes(Resource):
-    @api.marshal_with(care_fields)
+    @ns.marshal_with(model_care)
+    @ns.doc('care_types')
     def get(self):
         query = session.query(PlantCareType)
         response = [row.to_json() for row in query]
         return response
 
 
-@api.route('/Care')
+@ns.route('/Care/<user_id>')
+@ns.doc(params={'user_id':'A User ID'})
 class Care(Resource):
-    @api.marshal_with(model_nested)
-    def get(self):
+#    care_args = {'user_id': wa.fields.Int(required=True)}
+    @ns.doc('care_history')
+    @ns.marshal_with(model_nested)
+    def get(self,user_id):
 
-#        filters =  [PlantInventory.site_user_id==args["user_id"]]
-        filters =  [PlantInventory.site_user_id==1]
+        filters =  [PlantInventory.site_user_id==user_id]
 
         query = session.query(PlantHistory).\
             join(PlantInventory).\
